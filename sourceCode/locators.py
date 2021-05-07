c1ef8172bcbfcbce17d3b184c69fdfe7181bf06e
Lekhani Ray <lekhaniray@gmail.com>
2021-05-07 01:40:18 -0400

    commit

---
+++ sourceCode/locators.py
@@ -0,0 +1,5 @@
+class locators(object):
+    login_id = "login-username"
+    password_id = "login-password"
+    play_button_xpath = "//*[@id='main']/div/div[2]/div[2]/footer/div/div[2]/div/div[1]/button[3]"
+    dismiss_xpath = "/html/body/div[12]/div/div/div/div[2]/button[2]"
